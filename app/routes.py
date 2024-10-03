e568523949613ec574be82eadbf1aad143a8712a
Isa <isa_xo@hotmail.com>
2024-10-03 20:15:45 +0100

    Added delete task functionality

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, request, redirect, url_for
-from . import db, app  # Import the app instance here
+from . import db, app
 from .models import Task
 
 @app.route("/", methods=["GET", "POST"])
@@ -14,3 +14,10 @@ def home():
 
     tasks = Task.query.all()
     return render_template("home.html", tasks=tasks)
+
+@app.route("/delete/<int:task_id>", methods=["POST"])
+def delete_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for("home"))
